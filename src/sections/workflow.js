f289168bf67e5e0fcb84e6f283bde7c9d751c8f8
Abhishek <abhishekdhakad61@gmai.com>
2021-07-10 13:49:14 +0530

    workflow section

--- src/sections/workflow.js
+++ src/sections/workflow.js
@@ -36,7 +36,26 @@ const data = [
 
 export default function WorkFlow() {
   return (
-    <h1>Workflow</h1>
+    <section sx={styles.workflow}>
+      <Container>
+        <SectionHeader
+          slogan="WHAT'S THE FUNCTION"
+          title="Meet the feature of our product"
+          isWhite
+        />
+        <Grid sx={styles.grid}>
+          {data.map((item) => (
+            <Box sx={styles.card} key={item.id}>
+              <Box sx={styles.iconBox}>{`0${item.id}`}</Box>
+              <Box sx={styles.wrapper}>
+                <Heading sx={styles.wrapper.title}>{item.title}</Heading>
+                <Text sx={styles.wrapper.subTitle}>{item.text}</Text>
+              </Box>
+            </Box>
+          ))}
+        </Grid>
+      </Container>
+    </section>
   );
 }
 
@@ -53,6 +72,7 @@ const styles = {
   grid: {
     mb: -1,
     pt: 0,
+    mt: 50,
     gridGap: [
       '35px 0',
       null,
