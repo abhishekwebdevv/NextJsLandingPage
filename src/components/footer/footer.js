3a5b819ff38055622a9af659a07ae30340ddb156
Abhishek <abhishekdhakad61@gmai.com>
2021-07-11 14:42:23 +0530

    Complete

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -6,7 +6,29 @@ import FooterLogo from 'assets/logo.svg';
 
 export default function Footer() {
   return (
-    <h1>Footer</h1>
+    <footer sx={styles.footer}>
+      <Container>
+        <Box sx={styles.footer.footerBottomArea}>
+          <Link path="/">
+            <Image src={FooterLogo} />
+          </Link>
+          <Box sx={styles.footer.menus}>
+            <nav>
+              {data.menuItem.map((item, i) => (
+                <Link
+                  path={item.path}
+                  key={i}
+                  label={item.label}
+                  sx={styles.footer.link}
+                />
+              ))}
+            </nav>
+          </Box>
+          <Text sx={styles.footer.copyright}>{data.copyright}</Text>Copyright by{' '}
+          {new Date().getFullYear()} Company Name
+        </Box>
+      </Container>
+    </footer>
   );
 }
 
